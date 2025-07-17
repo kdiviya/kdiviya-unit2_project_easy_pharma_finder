17a0ca3e233c7d4fe6bf9c181a9f88ef6ad222f9
Kdiviya <eraiswaryabe@gmail.com>
2025-07-17 17:49:10 -0500

    updated the Postmapping in UserController

--- easy-pharma-finder/src/main/java/com/example/easy_pharma_finder/controllers/UserController.java
+++ easy-pharma-finder/src/main/java/com/example/easy_pharma_finder/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.example.easy_pharma_finder.models.User;
 import com.example.easy_pharma_finder.repositories.FamilyMemberRepository;
 import com.example.easy_pharma_finder.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,58 +25,28 @@ public class UserController {
     private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
 
     @PostMapping("/submit")
-    public ResponseEntity<User> registerUser(@RequestBody User user) {
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
         //Verify the duplicates in the user table based on "email" column.
         Optional<User> existingUser = userRepository.findByEmail((user.getEmail()));
         User savedUser;
-        //Check if user found with same email, update the details of the corresponding user.Else create new user
+        //Check if user already exist with same email, display the message.Else create new user.
         if (existingUser.isPresent()) {
-            savedUser = existingUser.get();
-            savedUser.setFirstName(user.getFirstName());
-            savedUser.setMiddleName(user.getMiddleName());
-            savedUser.setLastName(user.getLastName());
-            savedUser.setUserName(user.getUserName());
-            savedUser.setDob(user.getDob());
-            savedUser.setPassword(passwordEncoder.encode(user.getPassword()));
-            savedUser.setEmail(user.getEmail());
-            savedUser.setContactNo(user.getContactNo());
-            savedUser.setLastVisitedDate(user.getLastVisitedDate());
-            savedUser.setStreet(user.getStreet());
-            savedUser.setCity(user.getCity());
-            savedUser.setCountry(user.getCountry());
-            savedUser.setState(user.getState());
-            savedUser.setZipCode(user.getZipCode());
-            savedUser.setInsuranceNumber(user.getInsuranceNumber());
-            savedUser.setInsuranceProvider(user.getInsuranceProvider());
-            savedUser.setInsuranceType(user.getInsuranceType());
-            savedUser.setIsFamilyMember(user.getIsFamilyMember());
-            savedUser = userRepository.save(savedUser);
-        } else {
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            savedUser = userRepository.save(user);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User with same email already exist");
         }
+
         //check if family member exists for the primary user.
         if (user.getFamilyMembers() != null && !user.getFamilyMembers().isEmpty()) {
-            //Iterate over the loop for all the family members to check the family member already exist in the same table.
+            //Iterate over the loop for all the family members and stored all of them in the table.
             for (FamilyMember familyMember : user.getFamilyMembers()) {
-                List<FamilyMember> existingFamilyMember = familyMemberRepository.findByNameAndRelationshipAndDobAndUser(familyMember.getName(), familyMember.getRelationship(), familyMember.getDob(), savedUser);
-
-                //If the family members name, dob, relationship already exist - update the existing one, else - create new value.
-                if (!existingFamilyMember.isEmpty()) {
-                    for (FamilyMember existing : existingFamilyMember) {
-                        existing.setName(familyMember.getName());
-                        existing.setDob(familyMember.getDob());
-                        existing.setRelationship(familyMember.getRelationship());
-                        existing.setUser(savedUser);
-                        familyMemberRepository.save(existing);
-                    }
-                } else {
-                    familyMember.setUser(savedUser);
-                    familyMemberRepository.save(familyMember);
-                }
+                    familyMember.setUser(user);
             }
         }
-        return ResponseEntity.ok(savedUser);
+
+        //store all the new user details in the user table and store the encoded password instead of plain text.
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        savedUser = userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
 }
