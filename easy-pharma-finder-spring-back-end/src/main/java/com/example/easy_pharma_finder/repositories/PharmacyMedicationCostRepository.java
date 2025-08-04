1829f87f6b7b7ced99af48d48c2e31bb9bd23c0e
Kdiviya <eraiswaryabe@gmail.com>
2025-08-03 21:32:51 -0500

    completed Backend code V1.1

--- easy-pharma-finder-spring-back-end/src/main/java/com/example/easy_pharma_finder/repositories/PharmacyMedicationCostRepository.java
+++ easy-pharma-finder-spring-back-end/src/main/java/com/example/easy_pharma_finder/repositories/PharmacyMedicationCostRepository.java
@@ -2,8 +2,11 @@ package com.example.easy_pharma_finder.repositories;
 
 import com.example.easy_pharma_finder.models.PharmacyMedicationCost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface PharmacyMedicationCostRepository extends JpaRepository<PharmacyMedicationCost, Integer> {
 
     //Query the Pharmacy_medication_cost table and return all the values based on pharmacy id and medication id.
--- easy-pharma-finder-spring-back-end/src/main/java/com/example/easy_pharma_finder/repositories/PharmacyRepository.java
+++ easy-pharma-finder-spring-back-end/src/main/java/com/example/easy_pharma_finder/repositories/PharmacyRepository.java
@@ -2,8 +2,11 @@ package com.example.easy_pharma_finder.repositories;
 
 import com.example.easy_pharma_finder.models.Pharmacy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Integer> {
     List<Pharmacy> findByZipCode(String zipCode); //Query pharmacy table and return all the values based on Zipcode.
 }
